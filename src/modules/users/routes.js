e75b01db44d842d5b7fe978890237b986b8fa72b
Davidrg02 <davrodriguezgu@unal.edu.co>
2024-03-28 18:20:43 -0500

    manage error responses

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -18,7 +18,7 @@ async function all (req, res) {
         response.success(req, res, list, 200);
     }
     catch (error) {
-        response.error(req, res, 'Internal error', 500, error);
+        next(error);
     }
 };
 
@@ -27,32 +27,32 @@ async function one(req, res) {
         const user = await controller.one(req.params.id);
         response.success(req, res, user, 200);
     } catch (error) {
-        response.error(req, res, 'Internal error', 500, error);
+        next(error);
     }
 };
 
 async function insert(req, res) {
     try {
         const user = await controller.insert(req.body);
-        response.success(req, res, user, 201);
+        response.success(req, res, "Item creado satisfactoriamente", 201);
     } catch (error) {
-        response.error(req, res, 'Internal error', 500, error);
+        next(error);
     }
 };
 
 async function update(req, res) {
     try {
         const user = await controller.update(req.params.id, req.body);
-        response.success(req, res, user, 200);
+        response.success(req, res, "Item actualizado satisfactoriamente", 200);
     } catch (error) {
-        response.error(req, res, 'Internal error', 500, error);
+        next(error);
     }
 };
 
 async function remove(req, res, next) {
     try {
         const user = await controller.remove(req.params.id);
-        response.success(req, res, user, 200);
+        response.success(req, res, "Item eliminado satisfactoriamente", 200);
     } catch (error) {
         next(error);
     }
