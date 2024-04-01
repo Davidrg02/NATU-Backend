ec9de0faaacc80db4c013e84a1339c86dd721116
Nicolás Hernández <nihernandezv@unal.edu.co>
2024-03-31 21:35:15 -0500

    Creación modules/product

---
+++ src/modules/products/controller.js
@@ -0,0 +1,33 @@
+const db = require('../../db/mysql');
+
+const table = 'PRODUCTO';
+const idField = 'ID_PRODUCTO';
+
+function all() {
+    return db.all(table);
+}
+
+function one(id) { 
+    return db.one(table, id, idField);
+}
+
+function insert(data) {
+    return db.insert(table, data);
+}
+
+function update(id, data) {
+    return db.update(table, data, id, idField);
+}
+
+function remove(id) {
+    return db.remove(table, id, idField);
+}
+
+
+module.exports = {
+    all,
+    one,
+    insert,
+    update,
+    remove
+}
