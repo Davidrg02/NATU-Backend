3acca9180207fd3ef0ddaace607cdc0cbf7c9f1d
Davidrg02 <davrodriguezgu@unal.edu.co>
2024-04-02 23:17:48 -0500

    id generation

--- src/modules/compradores/controller.js
+++ src/modules/compradores/controller.js
@@ -1,8 +1,10 @@
 const db = require('../../db/mysql');
+const uuid = require('uuid');
 
 const table = 'COMPRADOR';
 const idField = 'ID_Comprador';
 const auth = require('../auth/controller');
+
 
 function all() {
     return db.all(table);
@@ -14,18 +16,21 @@ function one(id) {
 
 async function insert(data) {
 
+    const id = uuid.v4();
+    const numericID = parseInt(id.split('-')[0], 16)/2;
+
     await auth.insert({
-        ID_Usuario: data.ID_Comprador,
+        ID_Usuario: numericID,
         Correo_usuario: data.Correo_usuario,
         Contraseña_encriptada: data.Contraseña_encriptada
     });
 
     const comprador = {
-        ID_Comprador: data.ID_Comprador,
+        ID_Comprador: numericID,
         Nombres_comprador: data.Nombres_comprador,
         Dirección_comprador: data.Dirección_comprador,
         Teléfono_comprador: data.Teléfono_comprador,
-        ID_Usuario: data.ID_Comprador
+        ID_Usuario: numericID
     }
 
     const response = await db.insert(table, comprador);
