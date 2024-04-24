25941b7faee1e94b56264a160baaeb04db4fc8f3
Davidrg02 <davrodriguezgu@unal.edu.co>
2024-04-23 23:16:19 -0500

    categorias y vendedores crud

--- src/modules/vendedores/controller.js
+++ src/modules/vendedores/controller.js
@@ -1,7 +1,10 @@
 const db = require('../../db/mysql');
+const uuid = require('uuid');
 
 const table = 'VENDEDOR';
 const idField = 'ID_Vendedor';
+const auth = require('../auth/controller');
+const Direccion = require('../direcciones/controller');
 
 function all() {
     return db.all(table);
@@ -11,8 +14,35 @@ function one(id) {
     return db.one(table, id, idField);
 }
 
-function insert(data) {
-    return db.insert(table, data);
+async function insert(data) {
+    
+    const id = uuid.v4();
+    const numericID = parseInt(parseInt(id.split('-')[0], 16)/2);
+
+    await auth.insert({
+        ID_Usuario: numericID,
+        Correo_usuario: data.Correo_usuario,
+        Contraseña_encriptada: data.Contraseña_encriptada,
+        Rol_USUARIO_ID_Rol: 2
+    });
+
+    await Direccion.insert({
+        ID_Direccion: numericID,
+        Direccion: data.Direccion,
+        Descripcion_adicional: data.Descripcion_adicional,
+        MUNICIPIO_ID_Municipio: data.MUNICIPIO_ID_Municipio
+    });
+
+    const vendedor = {
+        ID_Vendedor: numericID,
+        Nombre_vendedor: data.Nombre_vendedor,
+        USUARIO_ID_Usuario: numericID,
+        DIRECCION_ID_Direccion: numericID
+    }
+
+    const response = await db.insert(table, vendedor);
+
+    return response;
 }
 
 function update(id, data) {
