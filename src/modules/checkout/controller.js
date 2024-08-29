ae931f9a44dbf94867d29c88929e380aa439c525
David <davrodriguezgu@unal.edu.co>
2024-08-29 04:36:53 -0500

    dfgvfedgv

--- src/modules/checkout/controller.js
+++ src/modules/checkout/controller.js
@@ -1,71 +1,67 @@
 const mercadopago = require('mercadopago');
-
 const db = require('../../db/mysql');
-
 const config = require('../../config');
+const uuid = require('uuid');
+const client = new mercadopago.MercadoPagoConfig({ accessToken: config.mercadopago.accessToken });
 
-const client = new mercadopago.MercadoPagoConfig({ accessToken: config.mercadopago.accessToken});
+const createOrder = async (req, res) => {
 
-const createOrder = async(req, res) => {
+    const id = uuid.v4();
+    const numericID = parseInt(parseInt(id.split('-')[0], 16)/2);
 
-    const preference = new mercadopago.Preference(client);
-    console.log(req.body);
-    preference.create({
-        body: {
-            items: [
-                {
-                    title: req.body.title,
-                    unit_price: req.body.unit_price,
-                    quantity: req.body.quantity,
-                }
-            ],
-            back_urls: {
-                success: config.mercadopago.success,
-                failure: config.mercadopago.failure,
-                pending: config.mercadopago.pending,
-            },
-            auto_return: 'approved'
-        }
-    })
-    .then(async response => {
+    try {
+        const preference = new mercadopago.Preference(client);
+        console.log(req.body);
+
+        const response = await preference.create({
+            body: {
+                items: [
+                    {
+                        title: req.body.title,
+                        unit_price: req.body.unit_price,
+                        quantity: req.body.quantity,
+                    }
+                ],
+                back_urls: {
+                    success: config.mercadopago.success,
+                    failure: config.mercadopago.failure,
+                    pending: config.mercadopago.pending,
+                },
+                auto_return: 'approved'
+            }
+        });
+
         console.log(response);
+
         
         const dataOrden = {
-            ID_Orden: response.collector_id,
+            ID_Orden: numericID,
             COMPRADOR_ID_Comprador: req.body.idComprador,
             FechaHora_orden: new Date()
         };
-        
+
         const dataDetalleOrden = {
-            ID_Detalle_Orden : response.collector_id,
-            FechaHora_orden : new Date(),
-            Cantidad : req.body.quantity,
-            Subtotal : req.body.subtotal,
-            Producto_ID_Producto : req.body.idProducto,
-            Orden_ID_Orden : response.collector_id,
-            ESTADO_ID_Estado : 1
+            ID_Detalle_Orden: numericID,
+            FechaHora_orden: new Date(),
+            Cantidad: req.body.quantity,
+            Subtotal: req.body.subtotal,
+            Producto_ID_Producto: req.body.idProducto,
+            Orden_ID_Orden: numericID,
+            ESTADO_ID_Estado: 1
         };
-    
-        try {
-            await db.insert('ORDEN', dataOrden);
-            await db.insert('DETALLE_ORDEN', dataDetalleOrden);
-        }
-        catch(error) {
-            res.send(error);
-        }
+
+        await db.insert('ORDEN', dataOrden);
+        await db.insert('DETALLE_ORDEN', dataDetalleOrden);
         
-        res.send(response);
-    })
-    .catch(error => {
-        res.send(error);
+        res.json(response); // Send the response after the database operations are successful
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: 'An error occurred while creating the order.' });
     }
-    );
 };
 
 const success = (req, res) => res.send('success');
-
 const failure = (req, res) => res.send('failure');
-
 const pending = (req, res) => res.send('pending');
 
 const webhook = (req, res) => {
